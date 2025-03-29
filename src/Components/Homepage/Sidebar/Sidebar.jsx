384ba56753dc4cc37129bec56203f308bdb7c3d6
Chris-Loza <christianloza99@gmail.com>
2025-03-28 17:32:42 -0700

    more basic structure in the sidebar

--- src/Components/Homepage/Sidebar/Sidebar.jsx
+++ src/Components/Homepage/Sidebar/Sidebar.jsx
@@ -4,8 +4,24 @@ import "./sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sidebarMain">
-      <div className="sidebar">Sidebar Categories</div>
-      <div className="socials">Social Links</div>
+      <div className="sidebar">
+        <div className="categoriesContainer">
+          <div className="title">Home</div>
+          <div className="categories">
+            <a href="">About</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+            <a href="">Education</a>
+          </div>
+        </div>
+      </div>
+      <div className="socials">
+        <div className="title">Socials</div>
+        <div className="links">
+          <a href="">LinkedIn</a>
+          <a href="">GitHub</a>
+        </div>
+      </div>
       <div className="hireMe">Hire Me</div>
     </div>
   );
