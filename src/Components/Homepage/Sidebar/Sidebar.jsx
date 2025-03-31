5e7981f1b2f1ad94fcdd430f8b3d1268028779c6
Chris-Loza <christianloza99@gmail.com>
2025-03-31 14:26:41 -0700

    added basic sidebar structure

--- src/Components/Homepage/Sidebar/Sidebar.jsx
+++ src/Components/Homepage/Sidebar/Sidebar.jsx
@@ -6,23 +6,35 @@ const Sidebar = () => {
     <div className="sidebarMain">
       <div className="sidebar">
         <div className="categoriesContainer">
-          <div className="title">Home</div>
+          <div className="title">
+            <p>Home</p>
+          </div>
           <div className="categories">
-            <a href="">About</a>
-            <a href="">Skills</a>
-            <a href="">Projects</a>
-            <a href="">Education</a>
+            <div className="buttons">
+              <a href="">About</a>
+              <a href="">Skills</a>
+              <a href="">Projects</a>
+              <a href="">Education</a>
+            </div>
           </div>
         </div>
       </div>
       <div className="socials">
-        <div className="title">Socials</div>
+        <div className="title">
+          <p>Socials</p>
+        </div>
         <div className="links">
           <a href="">LinkedIn</a>
           <a href="">GitHub</a>
+          <a href="">Resume</a>
         </div>
       </div>
-      <div className="hireMe">Hire Me</div>
+      <div className="hireMe">
+        <div className="textAndImageContainer">
+          <p>*</p>
+          <p>Hire Me</p>
+        </div>
+      </div>
     </div>
   );
 };
