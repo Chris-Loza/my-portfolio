ac30dbc79c67b0b15f8d44062722a876da0e60c6
Chris-Loza <christianloza99@gmail.com>
2025-04-01 15:02:03 -0700

    added css styling and relevant icons to Sidebar, need to do border animation for Hire Me next

--- src/Components/Homepage/Sidebar/Sidebar.jsx
+++ src/Components/Homepage/Sidebar/Sidebar.jsx
@@ -11,10 +11,34 @@ const Sidebar = () => {
           </div>
           <div className="categories">
             <div className="buttons">
-              <a href="">About</a>
-              <a href="">Skills</a>
-              <a href="">Projects</a>
-              <a href="">Education</a>
+              <a href="">
+                <img
+                  src="../../../../Images/AboutEEEEEEFill.svg"
+                  alt="About Icon"
+                />
+                <p>About</p>
+              </a>
+              <a href="">
+                <img
+                  src="../../../../Images/SkillsEEEEEEFill.png"
+                  alt="Skills Icon"
+                />
+                <p>Skills</p>
+              </a>
+              <a href="">
+                <img
+                  src="../../../../Images/ProjectsEEEEEEFill.png"
+                  alt="Projects Icon"
+                />
+                <p>Projects</p>
+              </a>
+              <a href="">
+                <img
+                  src="../../../../Images/EducationEEEEEEFill.png"
+                  alt="Education Icon"
+                />
+                <p>Education</p>
+              </a>
             </div>
           </div>
         </div>
@@ -24,9 +48,39 @@ const Sidebar = () => {
           <p>Socials</p>
         </div>
         <div className="links">
-          <a href="">LinkedIn</a>
-          <a href="">GitHub</a>
-          <a href="">Resume</a>
+          <a
+            href="https://www.linkedin.com/in/christianloza/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="../../../../Images/LinkedInEEEEEE.svg"
+              alt="LinkedIn Icon"
+            />
+            <p>LinkedIn</p>
+          </a>
+          <a
+            href="https://github.com/Chris-Loza"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="../../../../Images/WhiteGithubNoOutline.svg"
+              alt="GitHub Icon"
+            />
+            <p>GitHub</p>
+          </a>
+          <a
+            href="../../../../Images/Resume032525.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="../../../../Images/WhiteResumeFill.png"
+              alt="Resume Icon"
+            />
+            <p>Resume</p>
+          </a>
         </div>
       </div>
       <div className="hireMe">
