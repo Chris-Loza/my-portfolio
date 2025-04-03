42ae922ba4b29819b27a5a8e1ba6f77f3d1b27b4
Chris-Loza <christianloza99@gmail.com>
2025-04-03 13:07:22 -0700

    better stylings for the sidebar and added very basic structure to content window

--- src/Components/Homepage/MainContent/Content.jsx
+++ src/Components/Homepage/MainContent/Content.jsx
@@ -4,7 +4,12 @@ import "./content.css";
 const Content = () => {
   return (
     <div className="mainContentMain">
-      <div className="contentWindow"> Content Window</div>
+      <div className="contentWindow">
+        <div className="about category">About</div>
+        <div className="skills category">Skills</div>
+        <div className="projects category">Projects</div>
+        <div className="education category">Education</div>
+      </div>
       <div className="contentWindowBackground">
         <img src="../../../../public/Images/philipp-pilz-AOhBMkQlzgM-unsplash.jpg" alt="" />
       </div>
